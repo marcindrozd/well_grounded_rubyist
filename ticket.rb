ac376662144cb204ca3c5c93cdf194a255812a46
Marcin Drozd <marcindrozd@me.com>
2014-12-31 15:54:49 +0100

    Added ability to call methods by using 'send'

--- ticket.rb
+++ ticket.rb
@@ -28,3 +28,12 @@ puts "This ticket is for: #{ticket.event}, at #{ticket.venue}. " +
   "The performer is #{ticket.performer}. " +
   "The seat is #{ticket.seat}, " +
   "and it costs $#{"%.2f." % ticket.price}"
+
+puts "If you want to get specific information. Please enter them (e.g. seat, performer): "
+request = gets.chomp
+
+if ticket.respond_to?(request)
+  puts ticket.send(request)
+else
+  puts "No such information available."
+end
